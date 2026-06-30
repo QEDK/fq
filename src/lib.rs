2737374146063f5f0fb2e518ba2d6d056cd7d028
qedk <1994constant@gmail.com>
2026-06-30 17:20:21 +0400

    fix: fix rustdoc

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 /*!
 A fast and simple ring-buffer-based single-producer, single-consumer queue with no dependencies. You can use this to write Rust programs with low-latency message passing.
-```
 
 ## Quickstart
 ```rust
